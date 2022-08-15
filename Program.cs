0f3a88eff25ac3d96c80114999d7d537a7b6822a
NVA-HP <NVA-HP@NVA-HP>
2022-08-15 13:53:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+Console.WriteLine("Имя:");
+var name = Console.ReadLine();
+Console.WriteLine("Фамилия:");
+var surname = Console.ReadLine();
+int age;
+Console.WriteLine("Возраст:");
+while (!int.TryParse(Console.ReadLine(), out age))
+{
+    Console.WriteLine("Ошибка, ввведите числом");
+    Console.WriteLine();
+    Console.WriteLine("Возраст:");
+}
+
+Console.WriteLine("Возраст:",age);
+Console.WriteLine("Хобби:");
+var hobby = Console.ReadLine();
+Console.Write($" Результат: Имя:{name}, Фамилия: {surname},Возраст{age},Хобби:, {hobby}");
+
